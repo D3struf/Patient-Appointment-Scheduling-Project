f85f08b2e8d34f550fc7ec62102d48245624b257
D3struf <johnpaul.monter@tup.edu.ph>
2023-04-29 00:11:16 +0800

    Code and Payment Function na lang

--- header.h
+++ header.h
@@ -5,13 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <time.h>
 
 #define ACCOUNT_FILE "./database/account.txt"
 #define PATIENT_FILE "./database/patient.txt"
-// #define DEPARTMENTS ""
+#define DOCTOR_FILE "./database/doctor.txt"
+#define FOLDER "./database/Schedules/"
 #define ENTER 13
 #define TAB 9
 #define BKSP 8
+#define DAYS 3
+#define MAX_PATIENTS 20
+#define MAX_DOCTORS 2
 
 // ===============================
 // Array of Structure
@@ -23,14 +28,36 @@ typedef struct account {
     char sex[51];
     char bday[9];
     char contact_number[12];
+    char appointment_date[101];
+    char appointment_doctor[101];
+    char appointment_doctor_department[101];
+    char appointment_doctor_schedule[101];
+    char appointment_doctor_email[101];
+    char appointment_doctor_contact_number[12];
+    char appointment_code[5];
     int age;
 }ACCOUNT;
+
+typedef struct appointments {
+    char date[31];
+    int num_patients;
+}SLOT;
+
+typedef struct doctor {
+    char name[81];
+    char department[51];
+    char schedule[101];
+    char email[101];
+    char contact_number[12];
+}DOCTOR;
 
 // ===============================
 // Data Structure
 // ===============================
 typedef struct node {
     ACCOUNT accounts;
+    SLOT slots;
+    DOCTOR doctors;
     struct node *next;
 }LIST;
 
@@ -50,7 +77,7 @@ int menu(int x);
 // Account Related Functions
 // ===============================
 int login_Account();
-int add_Account(ACCOUNT x);
+int add_Account(ACCOUNT );
 char *get_Password(char *, char *);
 void display();
 int search_Account();
@@ -60,11 +87,17 @@ int search_Account();
 // ===============================
 void patient_Information();
 void display_Patient_Information();
+void appointment_Schedule();
+void view_Schedule();
+DOCTOR appoint_Doctor();
+char *appointment_code();
 
 // ===============================
 // Database Functions
 // ===============================
 void save();
 void retrieve();
+int check_Patients_Slot_File(char *);
+void save_Patients_Slot_File(char *, int );
 
 #endif // HEADER_H_INCLUDED
--- main.c
+++ main.c
@@ -6,10 +6,16 @@
 int main(void)
 {
     int choice1, choice2, login;
+    char folder_path[101];
+
+    // Create the folder path
+    snprintf(folder_path, sizeof(folder_path), "%s", FOLDER);
+    mkdir(folder_path, 0777); // full permission to all
 
     init();
     retrieve();
     display();
+
     // Ask for user Account
     while (1)
     {
@@ -36,10 +42,11 @@ int main(void)
         {
             case 1: display_Patient_Information(); 
                     break;
-            case 2: break;
-            case 3: break;
-            case 4: save(); 
-                    exit(0); 
+            case 2: appointment_Schedule();
+                    break;
+            case 3: view_Schedule();
+                    break;
+            case 4: 
                     break;
             case 5: save(); 
                     exit(0); 
@@ -49,8 +56,6 @@ int main(void)
                     break;
         }
     }
-    //display all account pa lang to
-    display();
     return 0;
 }
 
@@ -65,7 +70,8 @@ int login_Account()
     p = L;
     int tries = 3;
     char *pass;
-
+    
+    system("cls");
     printf("Enter Username: ");
     scanf(" %[^\n]s", y.username);
     strcpy(global_Username, y.username);
@@ -150,6 +156,7 @@ char *get_Password(char *string, char *PASSWORD)
 void patient_Information()
 {
     ACCOUNT x;
+    char *temp;
     system("cls");
     printf("\n\n\tPLEASE FILL UP THE FOLLOWING INFORMATION\n");
     
@@ -166,10 +173,14 @@ void patient_Information()
     scanf(" %[^\n]s", x.bday);
     printf("Enter Contact No: ");
     scanf(" %[^\n]s", x.contact_number);
-    printf("Enter Password: ");
-    scanf(" %[^\n]s", x.password);
+    temp = get_Password("Enter Password: ", x.password);
+    strcpy(x.password, temp);
+    strcpy(x.appointment_date, "\0");
+    strcpy(x.appointment_doctor, "\0");
+    strcpy(x.appointment_doctor_contact_number, "\0");
 
     add_Account(x);
+    printf("\nAccount Created Successfully!!\n");
     system("pause");
 }
 
@@ -224,6 +235,7 @@ void display()
 int menu(int x)
 {
     int choice;
+    system("cls");
     if (x == 1)
     {
         printf("[1] Log in Account\n");
@@ -256,6 +268,7 @@ void display_Patient_Information()
     {
         if (strcmp(p->accounts.username, global_Username) == 0)
         {
+            system("cls");
             printf("================================================================\n");
             printf("                     Patient Information                        \n");
             printf("================================================================\n");
@@ -275,6 +288,244 @@ void display_Patient_Information()
     }
 }
 
+void appointment_Schedule()
+{
+    SLOT x[3];
+    DOCTOR y;
+
+    system("cls");
+    printf("================================================================\n");
+    printf("                     Appointment Schedule                       \n");
+    printf("================================================================\n");
+
+    // Check if the user has already scheduled an appointment
+    LIST *q;
+    q = L;
+    while (q != NULL)
+    {
+        if (strcmp(q->accounts.username, global_Username) == 0)
+        {
+            if (strcmp(q->accounts.appointment_date, "\0") != 0)
+            {
+                printf("\nYou have already scheduled an appointment\n");
+                printf("Check your appointment schedule in the View Schedule Menu\n");
+                printf("================================================================\n");
+                system("pause");
+                return;
+            }
+            else
+            {
+                break;
+            }
+                
+        }
+    }
+
+    // get the current date and time
+    time_t ttime = time(NULL);
+
+    // Set the date to the start of the next day
+    struct tm *set_time = localtime(&ttime);
+    set_time->tm_hour = 0;
+    set_time->tm_min = 0;
+    set_time->tm_sec = 0;
+
+    // Iterate to generate the Appointment Schedules
+    for (int i = 0; i < DAYS; i++)
+    {
+        // Add 1 day to each iteration
+        set_time->tm_mday++;
+
+        //Convert the set_time to ttime
+        ttime = mktime(set_time);
+        set_time = localtime(&ttime);
+        // Format the date string
+        strftime(x[i].date, sizeof(x[i].date), "%b %d %Y", set_time);
+
+        // Check the file
+        x[i].num_patients = check_Patients_Slot_File(x[i].date);
+    }
+
+    printf("       DATE         NUMBER OF PATIENTS          SLOTS AVAILABLE \n\n");
+
+    // MENU for Appointment Dates
+    for (int i = 0, n = 1; i < DAYS; i++)
+    {
+        printf("[%i]    %s         %d                       (%d/%d)\n", n++, x[i].date, x[i].num_patients, x[i].num_patients, MAX_PATIENTS);
+    }
+
+    // Get the user input
+    int choice;
+    //char *code;
+    while (1)
+    {
+        int full = 0;
+        printf("\nEnter Choice: ");
+        scanf("%d", &choice);
+        printf("================================================================\n");
+        switch (choice)
+        {
+            case 1: 
+            case 2:
+            case 3: 
+                    if (x[choice-1].num_patients >= MAX_PATIENTS)
+                    {
+                        printf("Sorry, the slots are already full\n");
+                        printf("Please choose another date\n");
+                        full = 1;
+                        break;
+                    }
+                    else
+                    {
+                        y = appoint_Doctor();
+                        //code = appointment_code();
+                        //printf("Your Appointment Code is %s\n", code);
+                        printf("You have Successfully created an Appointment Schedule\n");
+                        printf("Your Appointment Schedule is on %s\n", x[choice-1].date);
+                        printf("Your Doctor is %s\n", y.name);
+                        
+                        x[choice-1].num_patients++;
+                        break;
+                    }
+                    
+            default: printf("Invalid Choice\n");
+                    break;
+        }
+        printf("================================================================\n");
+        system("pause");
+        if ((choice > 0 && choice <= 3) && full == 0)
+            break;
+    }
+
+    // Copy the date to the users data
+    LIST *p;
+    p = L;
+    while (p != NULL)
+    {
+        if (strcmp(p->accounts.username, global_Username) == 0)
+        {
+            strcpy(p->accounts.appointment_date, x[choice-1].date);
+            strcpy(p->accounts.appointment_doctor, y.name);
+            strcpy(p->accounts.appointment_doctor_department, y.department);
+            strcpy(p->accounts.appointment_doctor_schedule, y.schedule);
+            strcpy(p->accounts.appointment_doctor_email, y.email);
+            strcpy(p->accounts.appointment_doctor_contact_number, y.contact_number);
+            //strcpy(p->accounts.appointment_code, code);
+            break;
+        }
+    }
+
+    save_Patients_Slot_File(x[choice-1].date, x[choice-1].num_patients);
+}
+
+void save_Patients_Slot_File(char *DTIME, int x)
+{
+    FILE *outFile;
+    char file_path[101];
+    
+    // Format the file path
+    snprintf(file_path, sizeof(file_path), "%s%s", FOLDER, DTIME);
+
+    outFile = fopen(file_path, "w+");
+    if (outFile != NULL)
+    {
+        fprintf(outFile, "%d", x);
+        fclose(outFile);
+    }
+}
+
+int check_Patients_Slot_File(char *DTIME)
+{
+    FILE *inFile;
+    int num;
+    char file_path[101];
+
+    // Format the file path
+    snprintf(file_path, sizeof(file_path), "%s%s", FOLDER, DTIME);
+
+    inFile = fopen(file_path, "r");
+    if (inFile != NULL)
+    {
+        fscanf(inFile, "%d", &num);
+        return num;
+        fclose(inFile);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void view_Schedule()
+{
+    LIST *p;
+    p = L;
+    
+    while (p != NULL)
+    {
+        if (strcmp(p->accounts.username, global_Username) == 0)
+        {
+            system("cls");
+            printf("================================================================\n");
+            printf("                     Appointment Schedule                       \n");
+            printf("================================================================\n");
+            printf("USERNAME:           %s\n", p->accounts.username);
+            printf("NAME:               %s\n", p->accounts.name);
+            printf("APPOINTMENT DATE:   %s\n", p->accounts.appointment_date);
+            printf("===========================CODE: %s============================\n", p->accounts.appointment_code);
+            printf("\nDOCTOR DETAILS\n");
+            printf("APPOINTMENT DOCTOR: %s\n", p->accounts.appointment_doctor);
+            printf("DEPARTMENT:         %s\n", p->accounts.appointment_doctor_department);
+            printf("SCHEDULE:           %s\n", p->accounts.appointment_doctor_schedule);
+            printf("EMAIL:              %s\n", p->accounts.appointment_doctor_email);
+            printf("CONTACT NUMBER:     %s\n", p->accounts.appointment_doctor_contact_number);
+            printf("================================================================\n\t");
+            system("pause");
+            printf("================================================================\n");
+            break;
+        }
+        p = p->next;
+    }
+}
+
+DOCTOR appoint_Doctor()
+{
+    char *temp;
+    DOCTOR doctors[MAX_DOCTORS] = {
+        {"Dr. John Smith", "OPD", "Monday to Friday - 9:00am to 5:00pm", "john.smith@tupmc.com", "09123456789"},
+        {"Dr. Sarah Lee", "OPD", "Tuesday to Saturday - 10:00am to 6:00pm", "sarah.lee@hospital.com", "09987654321"}
+        };
+
+    // generate a number 
+    srand(time(NULL));
+    int num = rand() % MAX_DOCTORS ;
+
+    return doctors[num];
+}
+
+char *appointment_code()
+{
+    // FORMAT CODE: CDDD 
+    char code[5], 
+        *temp;
+    int num, num2 = 0;
+    
+    // generate a random number
+    srand(time(NULL));
+    num = rand() % 999 + 100;
+    while(num2 > 90 || num2 < 65)
+    {
+        num2 = rand() % 90;
+    }
+
+    // convert the number to character
+    sprintf(code, "%c%d", num2, num);
+    strcpy(temp, code);
+    printf("%s\n", temp);
+    system("pause");
+    return temp;
+}
+
 // ===============================================
 // DATABASE FUNCTIONS
 // ===============================================
@@ -286,12 +537,13 @@ void init()
 
 void save()
 {
-    FILE *outFile, *outFile2;
+    FILE *outFile, *outFile2, *outFile3;
 
     outFile = fopen(ACCOUNT_FILE, "w+");
     outFile2 = fopen(PATIENT_FILE, "w+");
+    outFile3 = fopen(DOCTOR_FILE, "w+");
 
-    if (outFile == NULL || outFile2 == NULL)
+    if (outFile == NULL || outFile2 == NULL || outFile3 == NULL)
     {
         printf("Error opening file\n");
         system("pause");
@@ -299,11 +551,13 @@ void save()
     else
     {
         fprintf(outFile, "Username,Password\n");
-        fprintf(outFile2, "Name,Age,Sex,Birthday,ContactNumber\n");
+        fprintf(outFile2, "Name,Sex,Birthday,Contact_Number,Appointment_Date,Age\n");
+        fprintf(outFile3, "Name,Department,Schedule,Email,Contact_Number\n");
         while (L != NULL)
         {
             fprintf(outFile, "%s,%s\n", L->accounts.username, L->accounts.password);
-            fprintf(outFile2, "%s,%s,%s,%s,%d\n", L->accounts.name, L->accounts.sex, L->accounts.bday, L->accounts.contact_number, L->accounts.age);
+            fprintf(outFile2, "%s,%s,%s,%s,%s,%d\n", L->accounts.name, L->accounts.sex, L->accounts.bday, L->accounts.contact_number, L->accounts.appointment_date, L->accounts.age);
+            fprintf(outFile3, "%s,%s,%s,%s,%s\n", L->accounts.appointment_doctor, L->accounts.appointment_doctor_department, L->accounts.appointment_doctor_schedule, L->accounts.appointment_doctor_email, L->accounts.appointment_doctor_contact_number);
             L = L->next;
         }
     }
@@ -313,15 +567,16 @@ void save()
 
 void retrieve()
 {
-    FILE *inFile, *inFile2;
+    FILE *inFile, *inFile2, *inFile3;
     ACCOUNT z;
     int i = 0;
-    char line[41], line2[81];
+    char line[41], line2[81], line3[101];
 
     inFile = fopen(ACCOUNT_FILE, "r+");
     inFile2 = fopen(PATIENT_FILE, "r+");
+    inFile3 = fopen(DOCTOR_FILE, "r+");
 
-    if (inFile == NULL || inFile2 == NULL)
+    if (inFile == NULL || inFile2 == NULL || inFile3 == NULL)
     {
         printf("Error opening file\n");
         system("pause");
@@ -334,12 +589,14 @@ void retrieve()
             {
                 fscanf(inFile, "%s\n", line);
                 fscanf(inFile2, "%s\n", line2);
+                fscanf(inFile3, "%s\n", line3);
                 i = 1;
             }
             else
             {
                 fscanf(inFile, " %[^,],%s\n", z.username, z.password);
-                fscanf(inFile2, " %[^,],%[^,],%[^,],%[^,],%d\n", z.name, z.sex, z.bday, z.contact_number, &z.age);
+                fscanf(inFile2, " %[^,],%[^,],%[^,],%[^,],%[^,],%d\n", z.name, z.sex, z.bday, z.contact_number, z.appointment_date, &z.age);
+                fscanf(inFile3, " %[^,],%[^,],%[^,],%[^,],%[^,]\n", z.appointment_doctor, z.appointment_doctor_department, z.appointment_doctor_schedule, z.appointment_doctor_email, z.appointment_doctor_contact_number);
                 add_Account(z);
             }
         }
