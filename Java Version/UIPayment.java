1cf38cfe59adb823aa83076a1f76c993923834d6
jeannmaycarolino <jeannemay.carolino@tup.edu.ph>
2023-06-10 20:33:46 +0800

    UI ni Jeanne

---
+++ Java Version/UIPayment.java
@@ -0,0 +1,136 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UIPayment extends JFrame implements ActionListener {
+
+    public static void main(String[] args) {
+        new UIPayment();
+    }
+
+    UIPayment() {
+
+        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("images/logo.png"));
+        ImageIcon bgImage = new ImageIcon(getClass().getClassLoader().getResource("images/payment.png"));
+        // Resize Image
+        Image tempImage = bgImage.getImage();
+        Image tempImage2 = tempImage.getScaledInstance(1366, 730, Image.SCALE_SMOOTH);
+        bgImage = new ImageIcon(tempImage2);
+
+        // Labels for Image
+        JLabel bgImageLabel = new JLabel();
+        bgImageLabel.setIcon(bgImage);
+        bgImageLabel.setBounds(0, -20, 1366, 768);
+
+        // JLayered for layers
+        JLayeredPane bgImageLayer = new JLayeredPane();
+        bgImageLayer.setBounds(0, 0, 1366, 768);
+        bgImageLayer.add(bgImageLabel, JLayeredPane.DEFAULT_LAYER);
+
+        JButton button = new JButton();
+        button.setText("Patient Information");
+        button.setFont(new Font("Dialog", Font.BOLD, 18));
+        button.setForeground(Color.WHITE);
+        button.setBackground(Color.BLUE);
+        button.setOpaque(true);
+        button.setFocusable(false);
+        button.addActionListener(this);
+        button.setBounds(60, 315, 250, 50);
+
+        JButton button1 = new JButton();
+        button1.setText("Schedule");
+        button1.setFont(new Font("Dialog", Font.BOLD, 18));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.BLUE);
+        button1.setOpaque(true);
+        button1.setFocusable(false);
+        button1.addActionListener(this);
+        button1.setBounds(60, 384, 250, 50);
+
+        JButton button2 = new JButton();
+        button2.setText("Payment");
+        button2.setFont(new Font("Dialog", Font.BOLD, 18));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(new Color(0x698f9a));
+        button2.setOpaque(true);
+        button2.setFocusable(false);
+        button2.addActionListener(this);
+        button2.setBounds(60, 452, 250, 50);
+
+        JButton button3 = new JButton();
+        button3.setText("Back");
+        button3.setFont(new Font("Dialog", Font.BOLD, 18));
+        button3.setForeground(Color.WHITE);
+        button3.setBackground(new Color(0xD62839));
+        button3.setOpaque(true);
+        button3.setFocusable(false);
+        button3.addActionListener(this);
+        button3.setBounds(60, 655, 250, 50);
+
+        JButton cash = new JButton();
+        cash.setText("Cash");
+        cash.setFont(new Font("Dialog", Font.BOLD, 18));
+        cash.setForeground(Color.WHITE);
+        cash.setBackground(new Color(0x698f9a));
+        cash.setOpaque(true);
+        cash.setFocusable(false);
+        cash.addActionListener(this);
+        cash.setBounds(534, 452, 230, 50);
+        cash.setBorder(BorderFactory.createEmptyBorder());
+
+        JButton ol = new JButton();
+        ol.setText("Online");
+        ol.setFont(new Font("Dialog", Font.BOLD, 18));
+        ol.setForeground(Color.WHITE);
+        ol.setBackground(new Color(0x698f9a));
+        ol.setOpaque(true);
+        ol.setFocusable(false);
+        ol.addActionListener(this);
+        ol.setBounds(996, 452, 230, 50);
+        ol.setBorder(BorderFactory.createEmptyBorder());
+
+        // Text Fields
+        JTextField username = new JTextField();
+        username.setBounds(1035, 23, 250, 35);
+        username.setFont(new Font("Dialog", Font.PLAIN, 18));
+        username.setForeground(Color.BLACK);
+        username.setBackground(new Color(0xe8eaec));
+        username.setBorder(BorderFactory.createEmptyBorder());
+
+       // Style the Frame
+        this.setIconImage(icon.getImage());
+        this.setTitle("Patient Appointment Scheduling System");
+        this.getContentPane().setBackground(new Color(0xFFFFFF));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        centerFrameOnScreen(this);
+        this.setLayout(null);
+        this.setSize(1366, 768);
+        this.setVisible(true);
+        this.setResizable(false);
+
+        // add objects
+        this.add(bgImageLayer);
+        bgImageLayer.add(button, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button1, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button2, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button3, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(cash, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(ol, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(username, JLayeredPane.PALETTE_LAYER);
+    }
+
+    private static void centerFrameOnScreen(JFrame frame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = (screenSize.width - frame.getWidth()) / 8;
+        int centerY = (screenSize.height - frame.getHeight()) / 8;
+        frame.setLocation(centerX, centerY);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+}
