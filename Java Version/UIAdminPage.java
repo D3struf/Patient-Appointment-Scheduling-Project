738380e5be5599627c68fd66528523488235db8c
jeannmaycarolino <jeannemay.carolino@tup.edu.ph>
2023-06-11 21:17:37 +0800

    UI ni Jeanne

---
+++ Java Version/UIAdminPage.java
@@ -0,0 +1,121 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UIHomePage extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        new UIHomePage();
+    }
+
+    // Frame
+    UIHomePage() {
+
+        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("images/logo.png"));
+        ImageIcon bgImage = new ImageIcon(getClass().getClassLoader().getResource("images/bghomepage.png"));
+
+        // Resize Image
+        Image tempImage = bgImage.getImage();
+        Image tempImage2 = tempImage.getScaledInstance(1366, 730, Image.SCALE_SMOOTH);
+        bgImage = new ImageIcon(tempImage2);
+
+        // Labels for Image
+        JLabel bgImageLabel = new JLabel();
+        bgImageLabel.setIcon(bgImage);
+        bgImageLabel.setBounds(0, -20, 1366, 768);
+
+        JButton button = new JButton();
+        button.setText("Patient Information");
+        button.setFont(new Font("Dialog", Font.BOLD, 18));
+        button.setForeground(Color.WHITE);
+        button.setBackground(Color.BLUE);
+        button.setOpaque(true);
+        button.setFocusable(false);
+        button.addActionListener(this);
+        button.setBounds(60, 315, 250, 50);
+
+        JButton button1 = new JButton();
+        button1.setText("Schedule");
+        button1.setFont(new Font("Dialog", Font.BOLD, 18));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.BLUE);
+        button1.setOpaque(true);
+        button1.setFocusable(false);
+        button1.addActionListener(this);
+        button1.setBounds(60, 384, 250, 50);
+
+        JButton button2 = new JButton();
+        button2.setText("Payment");
+        button2.setFont(new Font("Dialog", Font.BOLD, 18));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(Color.BLUE);
+        button2.setOpaque(true);
+        button2.setFocusable(false);
+        button2.addActionListener(this);
+        button2.setBounds(60, 452, 250, 50);
+
+        JButton button3 = new JButton();
+        button3.setText("Logout");
+        button3.setFont(new Font("Dialog", Font.BOLD, 18));
+        button3.setForeground(Color.WHITE);
+        button3.setBackground(new Color(0xD62839));
+        button3.setOpaque(true);
+        button3.setFocusable(false);
+        button3.addActionListener(this);
+        button3.setBounds(60, 655, 250, 50);
+
+        JTextField username = new JTextField();
+        username.setBounds(1035, 23, 250, 35);
+        username.setFont(new Font("Dialog", Font.PLAIN, 18));
+        username.setForeground(Color.BLACK);
+        username.setBackground(new Color(0xe8eaec));
+        username.setBorder(BorderFactory.createEmptyBorder());
+
+        JTextField sched = new JTextField();
+        sched.setBounds(445, 480, 380, 35);
+        sched.setFont(new Font("Dialog", Font.PLAIN, 18));
+        sched.setForeground(Color.BLACK);
+        sched.setBackground(new Color(0x698f9a));
+        sched.setCaretColor(Color.WHITE);
+        sched.setBorder(BorderFactory.createEmptyBorder());
+
+        // JLayered for layers
+        JLayeredPane bgImageLayer = new JLayeredPane();
+        bgImageLayer.setBounds(0, 0, 1366, 768);
+        bgImageLayer.add(bgImageLabel, JLayeredPane.DEFAULT_LAYER);
+
+        // Style the Frame
+        this.setIconImage(icon.getImage());
+        this.setTitle("Patient Appointment Scheduling System");
+        this.getContentPane().setBackground(new Color(0xFFFFFF));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        centerFrameOnScreen(this);
+        this.setLayout(null);
+        this.setSize(1366, 768);
+        this.setVisible(true);
+        this.setResizable(false);
+
+        // add Objects
+        this.add(bgImageLayer);
+        bgImageLayer.add(button, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button1, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button2, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(button3, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(username, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(sched, JLayeredPane.PALETTE_LAYER);
+    }
+
+    private static void centerFrameOnScreen(JFrame frame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = (screenSize.width - frame.getWidth()) / 8;
+        int centerY = (screenSize.height - frame.getHeight()) / 8;
+        frame.setLocation(centerX, centerY);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+}
