a01117b71adc44165c4120cec8601b19e1ff5294
jeannmaycarolino <jeannemay.carolino@tup.edu.ph>
2023-06-13 13:11:21 +0800

    UI ni Jeanne

--- Java Version/UIPatientList.java
+++ Java Version/UIPatientList.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -64,6 +66,49 @@ public class UIPatientList extends JFrame implements ActionListener {
         logoutButton.addActionListener(this);
         logoutButton.setBounds(60, 655, 250, 50);
 
+        //set Table
+        JTable table = new JTable();
+        Object[] columnNames = {"Code", "Name", "Age", "Sex", "Birthday", "Contact No.", "Date", "Assigned Doctor", "Payment Status"};
+        DefaultTableModel model = new DefaultTableModel();
+
+        //set columns
+        model.setColumnIdentifiers(columnNames);
+        table.setModel(model);
+
+        //set table
+        table.setRowHeight(30);
+        table.setBackground(Color.white);
+        table.setForeground(Color.black);
+        table.setColumnSelectionAllowed(false);
+        table.setRowSelectionAllowed(false);
+        table.setFont(new Font("Dialog", Font.PLAIN, 12));
+        table.setAutoCreateRowSorter(true);
+        table.setEnabled(false);
+        table.setCellSelectionEnabled(false);
+        table.setDefaultEditor(getClass(), null);
+        table.getTableHeader().setReorderingAllowed(false);
+        table.getTableHeader().setResizingAllowed(false);
+        table.getTableHeader().setFont(new Font("Dialog", Font.BOLD, 14));
+        table.getTableHeader().getColumnModel().getColumn(0).setPreferredWidth(25);
+        table.getTableHeader().getColumnModel().getColumn(1).setPreferredWidth(132);
+        table.getTableHeader().getColumnModel().getColumn(2).setPreferredWidth(17);
+        table.getTableHeader().getColumnModel().getColumn(3).setPreferredWidth(17);
+        table.getTableHeader().getColumnModel().getColumn(4).setPreferredWidth(132);
+        table.getTableHeader().getColumnModel().getColumn(5).setPreferredWidth(96);
+        table.getTableHeader().getColumnModel().getColumn(6).setPreferredWidth(132);
+        table.getTableHeader().getColumnModel().getColumn(7).setPreferredWidth(115);
+        table.getTableHeader().getColumnModel().getColumn(8).setPreferredWidth(110);
+
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.getViewport().setBackground(Color.white);
+        scrollPane.setBounds(408,300,932,391);
+
+        Object[] row = new Object[4];
+        row[1] = "Jeanne May Carolino";
+        model.addRow(row);
+        model.addRow(row);
+
         // JLayered for layers
         JLayeredPane bgImageLayer = new JLayeredPane();
         bgImageLayer.setBounds(0, 0, 1366, 768);
@@ -87,6 +132,7 @@ public class UIPatientList extends JFrame implements ActionListener {
         bgImageLayer.add(doctorButton, JLayeredPane.PALETTE_LAYER);
         bgImageLayer.add(securityButton, JLayeredPane.PALETTE_LAYER);
         bgImageLayer.add(logoutButton, JLayeredPane.PALETTE_LAYER);
+        bgImageLayer.add(scrollPane, JLayeredPane.PALETTE_LAYER);
     }
 
     private static void centerFrameOnScreen(JFrame frame) {
